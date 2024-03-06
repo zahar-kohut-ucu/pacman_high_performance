5bdba125ea2eb3984af085c4d46404e4b4333830
Zahar Kohut <zaharentv2005@gmail.com>
2024-03-06 23:57:23 +0200

    algorithms dont crash (probably), seed problem fixed

--- src/main.py
+++ src/main.py
@@ -6,19 +6,19 @@ import creatures
 import movingAlgorithms
 import time
 
-m = 21 # Number of rcolumns
-n = 21 # Number of rows
+m = 50 # Number of columns
+n = 50 # Number of rows
 k = 4  # Number of ghosts and dots
 
 def main():
-    myMaze = maze.Maze(m, n, k, setSeed = True, seed = 18)
+    myMaze = maze.Maze(m, n, k, setSeed = True, seed = 13)
     tileSize = 20
     width = myMaze.trueN
     height = myMaze.trueM
-    
+
     pygame.font.init()
-    font = pygame.font.SysFont('freesansbold.ttf', 24) 
-    
+    font = pygame.font.SysFont('freesansbold.ttf', 24)
+
     screen = pygame.display.set_mode(((width + 2) * tileSize, (height + 5) * tileSize))
     pygame.display.set_caption("Pacman")
 
@@ -35,31 +35,36 @@ def main():
     pygame.display.flip()
 
     GAME = True
-    DELAY = 0.1
+    DELAY = 0.05
+    GHOST_MOVE_SWITCH = 1
 
     while GAME:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                
+
         time.sleep(DELAY)
         oldPacmans = [p.getCoordinates() for p in pacmans]
-        oldGhosts = [g.getCoordinates() for g in ghosts]
+        if GHOST_MOVE_SWITCH:
+            oldGhosts = [g.getCoordinates() for g in ghosts]
+        else:
+            oldGhosts = []
         newPacmanPositions = []
         newGhostsPosition = []
 
         for pacman in pacmans:
             newPacmanPositions.append(movingAlgorithms.getNextPacmanMove(myMaze, pacman))
-        for ghost in ghosts:    
+        for ghost in ghosts:
             newGhostsPosition.append(movingAlgorithms.getGhostNextMove(myMaze, ghost, pacmans))
-        
+
         myMaze.changePacmansCoordinates(pacmans, newPacmanPositions)
         for pacman in pacmans:
             move = myMaze.checkMove(pacman)
             if move == 0:
                 GAME = False
-        myMaze.changeGhostsCoordinates(ghosts, newGhostsPosition)
-        
+        if GHOST_MOVE_SWITCH:
+            myMaze.changeGhostsCoordinates(ghosts, newGhostsPosition)
+
         visualization.redrawBoard(screen, myMaze, pacmans, ghosts, oldGhosts + oldPacmans)
         for pacman in pacmans:
             move = myMaze.checkMove(pacman)
@@ -67,18 +72,22 @@ def main():
                 GAME = False
             elif move == 1:
                 myMaze.eatAndRespawnDot(pacman)
-        
+
+        if GHOST_MOVE_SWITCH:
+            GHOST_MOVE_SWITCH = 0
+        else:
+            GHOST_MOVE_SWITCH = 1
         text = font.render("Pac 1: " + str(pacmans[0].getPoints()) + " Pac 2: " + str(pacmans[1].getPoints()), True, (0, 255, 0))
-        refresher = pygame.Rect(0, (height + 3) * tileSize, tileSize * width, tileSize * 2) 
+        refresher = pygame.Rect(0, (height + 3) * tileSize, tileSize * width, tileSize * 2)
         pygame.draw.rect(screen, (0, 0, 0), refresher)
         screen.blit(text, ((width-4) * tileSize // 2, (height + 3) * tileSize))
         pygame.display.flip()
 
     print(f"Yellow : {pacmans[0].getPoints()} Red : {pacmans[1].getPoints()}")
-    time.sleep(5)
+    time.sleep(3)
     pygame.quit()
 
-        
+
 if __name__ == '__main__':
     main()
 
